bd7dc93b973723ff8655da6e2a6748b08ae75eb8
yuzhang <yuzhang@coupang.com>
2020-09-04 16:58:24 +0800

    ...0.1.3

--- CMGLivePodDemo/Classes/VC.m
+++ CMGLivePodDemo/Classes/VC.m
@@ -7,41 +7,42 @@
 //
 
 #import "VC.h"
-#import <SuperPlayer/SuperPlayer.h>
+//#import <SuperPlayer/SuperPlayer.h>
 
-@interface VC ()<SuperPlayerDelegate>
-
-@property (nonatomic,strong) SuperPlayerView *playerView;
-
-@property (nonatomic,strong) SuperPlayerModel *playerModel;
+@interface VC ()
+//<SuperPlayerDelegate>
+//
+//@property (nonatomic,strong) SuperPlayerView *playerView;
+//
+//@property (nonatomic,strong) SuperPlayerModel *playerModel;
 
 @end
 
 @implementation VC
 
-- (SuperPlayerView *)playerView{
-    
-    if (!_playerView) {
-        _playerView = [[SuperPlayerView alloc] initWithFrame:CGRectMake(0,
-                                                                        0,
-                                                                        self.view.frame.size.width,
-                                                                        self.view.frame.size.height)];
-        _playerView.delegate = self;
-        _playerView.fatherView = self.view;
-    }
-    
-    return _playerView;;
-}
-
-- (SuperPlayerModel *)playerModel{
-    
-    if (!_playerModel) {
-        _playerModel = [[SuperPlayerModel alloc] init];
-        _playerModel.videoURL = @"http://liteavapp.qcloud.com/live/liteavdemoplayerstreamid.flv";
-    }
-    
-    return _playerModel;
-}
+//- (SuperPlayerView *)playerView{
+//
+//    if (!_playerView) {
+//        _playerView = [[SuperPlayerView alloc] initWithFrame:CGRectMake(0,
+//                                                                        0,
+//                                                                        self.view.frame.size.width,
+//                                                                        self.view.frame.size.height)];
+//        _playerView.delegate = self;
+//        _playerView.fatherView = self.view;
+//    }
+//
+//    return _playerView;;
+//}
+//
+//- (SuperPlayerModel *)playerModel{
+//
+//    if (!_playerModel) {
+//        _playerModel = [[SuperPlayerModel alloc] init];
+//        _playerModel.videoURL = @"http://liteavapp.qcloud.com/live/liteavdemoplayerstreamid.flv";
+//    }
+//
+//    return _playerModel;
+//}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -49,36 +50,36 @@
     
     self.view.backgroundColor = [UIColor redColor];
     
-    [self.playerView playWithModel:self.playerModel];
+//    [self.playerView playWithModel:self.playerModel];
 }
 
 
-#pragma mark - SuperPlayerDelegate
-/// 返回事件
-- (void)superPlayerBackAction:(SuperPlayerView *)player{
-    
-    
-}
-/// 全屏改变通知
-- (void)superPlayerFullScreenChanged:(SuperPlayerView *)player{
-    
-    
-}
-/// 播放开始通知
-- (void)superPlayerDidStart:(SuperPlayerView *)player{
-    
-    
-}
-/// 播放结束通知
-- (void)superPlayerDidEnd:(SuperPlayerView *)player{
-    
-    
-}
-/// 播放错误通知
-- (void)superPlayerError:(SuperPlayerView *)player errCode:(int)code errMessage:(NSString *)why{
-    
-    
-}
-// 需要通知到父view的事件在此添加
+//#pragma mark - SuperPlayerDelegate
+///// 返回事件
+//- (void)superPlayerBackAction:(SuperPlayerView *)player{
+//    
+//    
+//}
+///// 全屏改变通知
+//- (void)superPlayerFullScreenChanged:(SuperPlayerView *)player{
+//    
+//    
+//}
+///// 播放开始通知
+//- (void)superPlayerDidStart:(SuperPlayerView *)player{
+//    
+//    
+//}
+///// 播放结束通知
+//- (void)superPlayerDidEnd:(SuperPlayerView *)player{
+//    
+//    
+//}
+///// 播放错误通知
+//- (void)superPlayerError:(SuperPlayerView *)player errCode:(int)code errMessage:(NSString *)why{
+//    
+//    
+//}
+//// 需要通知到父view的事件在此添加
 
 @end
