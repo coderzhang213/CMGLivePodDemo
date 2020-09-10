1af4e8c7dc603a1b2bc36bb581e4fbd5b24a8617
yuzhang <yuzhang@coupang.com>
2020-09-10 14:44:44 +0800

    0.1.24

---
+++ CMGLivePodDemo/Classes/LiveFramework.framework/Headers/CMGVideoVC.h
@@ -0,0 +1,16 @@
+//
+//  CMGVideoVC.h
+//  AFNetworking
+//
+//  Created by yuzhang on 2020/9/10.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CMGVideoVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CMGLivePodDemo/Classes/LiveFramework.framework/Headers/LiveFramework.h
@@ -0,0 +1,20 @@
+//
+//  LiveFramework.h
+//  LiveFramework
+//
+//  Created by yuzhang on 2020/9/10.
+//  Copyright © 2020 yuzhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LiveFramework.
+FOUNDATION_EXPORT double LiveFrameworkVersionNumber;
+
+//! Project version string for LiveFramework.
+FOUNDATION_EXPORT const unsigned char LiveFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiveFramework/PublicHeader.h>
+#import <LiveFramework/CMGVideoVC.h>
+
+
